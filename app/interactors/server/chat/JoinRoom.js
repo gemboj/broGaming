0e1b30ce1b3b0075ce51454e344777dacb1b8493
gemboj <gemboj99@gmail.com>
2015-08-22 19:26:06 +0200

    join room refactor

--- app/interactors/server/chat/JoinRoom.js
+++ app/interactors/server/chat/JoinRoom.js
@@ -1,19 +1,23 @@
-function JoinRoom(usernameJoinsRoomid, getUsersInRoomId, send){
+function JoinRoom(transaction, usernameJoinsRoomid, getUsersInRoomId, send){
     var that = this;
+
 
     that.do = function(username, roomId){
         roomId = roomId === undefined ? 0 : roomId;
-        return usernameJoinsRoomid(username, roomId)
-            .then(function(){
-                return getUsersInRoomId(roomId);
-            })
+
+        return transaction(function(t){
+            return usernameJoinsRoomid(username, roomId, t)
+                .then(function(){
+                    return getUsersInRoomId(roomId, t);
+                })
+        })
             .then(function(users){
                 var data = {};
-                data[roomId] = users
+                data[roomId] = users;
                 send(username, 'roomUsers', data);
             })
             .catch(function(err){
-                console.log(err);
+                throw err;
             });
     }
 }
--- appUnitTests/interactors/server/chat/JoinRoomTest.js
+++ appUnitTests/interactors/server/chat/JoinRoomTest.js
@@ -2,34 +2,56 @@ describe('JoinRoom', function(){
     beforeEach(function(){
         var that = this;
         this.users = [];
+        this.transactionObj = {};
 
+        var that = this;
+        this.transaction = function(func){
+            return func(that.transactionObj)
+                .then(function(data){
+                    //commit
+                    return data;
+                })
+                .catch(function(err){
+                    //rollback
+                    throw err;
+                })
+        };
 
-        this.loggedusernameJoinsRoomid = function(){
+        this.usernameJoinsRoomid = function(username, roomid, t){
             return Promise.resolve();
         };
 
-        this.getLoggedUsersInRoomid = function(){
+        this.usernameJoinsRoomidReject = function(username, roomid, t){
+            return Promise.reject();
+        };
+
+        this.getUsernamesInRoomid = function(roomid, t){
             return Promise.resolve(that.users);
         };
 
-        this.sendLoggedUsersList = function(users){
+        this.sendUsernamesList = function(users){
 
         };
 
-        spyOn(this, 'loggedusernameJoinsRoomid').and.callThrough();
-        spyOn(this, 'getLoggedUsersInRoomid').and.callThrough();
-        spyOn(this, 'sendLoggedUsersList');
+        spyOn(this, 'transaction').and.callThrough();
+        spyOn(this, 'usernameJoinsRoomid').and.callThrough();
+        spyOn(this, 'getUsernamesInRoomid').and.callThrough();
+        spyOn(this, 'sendUsernamesList');
 
-        this.joinRoom = new JoinRoom(this.loggedusernameJoinsRoomid, this.getLoggedUsersInRoomid, this.sendLoggedUsersList);
+
     });
 
-    it('joins choosed room and sends users in that room', function(done){
+    it('joins choosed room and sends usernames in that room', function(done){
         var that = this;
+
+        this.joinRoom = new JoinRoom(this.transaction, this.usernameJoinsRoomid, this.getUsernamesInRoomid, this.sendUsernamesList);
 
         this.joinRoom.do('username', 2)
             .then(function(){
-                expect(that.loggedusernameJoinsRoomid).toHaveBeenCalledWith('username', 2);
-                expect(that.sendLoggedUsersList).toHaveBeenCalled();
+                expect(that.transaction).toHaveBeenCalled();
+                expect(that.usernameJoinsRoomid).toHaveBeenCalledWith('username', 2, that.transactionObj);
+                expect(that.getUsernamesInRoomid).toHaveBeenCalledWith(2, that.transactionObj);
+                expect(that.sendUsernamesList).toHaveBeenCalledWith('username', 'roomUsers', {2: that.users});
                 done();
             })
             .catch(function(){
@@ -40,14 +62,35 @@ describe('JoinRoom', function(){
     it('joins room 0 when roomId is undefined', function(done){
         var that = this;
 
+        this.joinRoom = new JoinRoom(this.transaction, this.usernameJoinsRoomid, this.getUsernamesInRoomid, this.sendUsernamesList);
+
         this.joinRoom.do('username')
             .then(function(){
-                expect(that.loggedusernameJoinsRoomid).toHaveBeenCalledWith('username', 0);
-                expect(that.sendLoggedUsersList).toHaveBeenCalled();
+                expect(that.transaction).toHaveBeenCalled();
+                expect(that.usernameJoinsRoomid).toHaveBeenCalledWith('username', 0, that.transactionObj);
+                expect(that.getUsernamesInRoomid).toHaveBeenCalledWith(0, that.transactionObj);
+                expect(that.sendUsernamesList).toHaveBeenCalledWith('username', 'roomUsers', {0: that.users});
                 done();
             })
             .catch(function(){
                 done.fail();
+            });
+    })
+
+    it('wont send users when transaction fails', function(done){
+        var that = this;
+
+        this.joinRoom = new JoinRoom(this.transaction, this.usernameJoinsRoomidReject, this.getUsernamesInRoomid, this.sendUsernamesList);
+
+        this.joinRoom.do('username')
+            .then(function(){
+
+                done.fail();
+            })
+            .catch(function(){
+                expect(that.sendUsernamesList).not.toHaveBeenCalled();
+
+                done();
             });
     })
 });
