9ef4fa5e90b36c6aae4082a8bf6587e04c2ed6d4
gemboj <gemboj99@gmail.com>
2016-04-05 00:35:32 +0200

    made ObjectPosition to be an interface

--- apps/threejsTest/javascript/server/ObjectPosition.js
+++ apps/threejsTest/javascript/server/ObjectPosition.js
@@ -3,5 +3,9 @@ function ObjectPosition(positionVector){
 }
 
 ObjectPosition.prototype.move = function(positionVector){
+    throw "calling interface method";
+};
 
+ObjectPosition.prototype.add = function(positionVector){
+    throw "calling interface method";
 };
