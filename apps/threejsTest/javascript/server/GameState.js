eb724d4fe563648ca9f76de8e4c285cf1e644428
gemboj <gemboj99@gmail.com>
2016-04-08 23:44:42 +0200

    added player support to GameState

--- apps/threejsTest/javascript/server/GameState.js
+++ apps/threejsTest/javascript/server/GameState.js
@@ -1,32 +1,66 @@
 function GameState(){
     this.objects = {};
+    this.players = {};
 }
 
 GameState.prototype.addObject = function(object){
     this.objects[object.getId()] = object;
 };
 
+GameState.prototype.addPlayer = function(player){
+    this.players[player.getId()] = player;
+};
+
 GameState.prototype.serialize = function(){
-    var currentGameState = {objects: {}};
+    var currentGameState = {
+        objects: this.serializeObjects(),
+        players: this.serializePlayers()
+    };
+
+    return currentGameState;
+};
+
+GameState.prototype.serializeObjects = function(){
+    var objects = {};
 
     for(var objectIndex in this.objects){
         var object = this.objects[objectIndex],
-            objectName = object.getId();
+            objectId = object.getId();
 
-        currentGameState.objects[objectName] = {
-            id: objectName,
+        objects[objectId] = {
+            id: objectId,
             position: object.getPosition().serialize()
         }
     }
 
-    return currentGameState;
+    return objects;
+};
+
+GameState.prototype.serializePlayers = function(){
+    var players = {};
+
+    for(var playerIndex in this.players){
+        var player = this.players[playerIndex],
+            playerId = player.getId();
+
+        players[playerId] = {
+            id: playerId,
+            position: player.getPosition().serialize()
+        }
+    }
+
+    return players;
 };
 
 GameState.prototype.updateObject = function(specification){
     this.objects[specification.id].setPosition(specification.position);
 };
 
-GameState.prototype.updateAllObjects = function(){
+GameState.prototype.updatePlayer = function(specification){
+    this.players[specification.id].setPosition(specification.position);
+};
+
+GameState.prototype.updateAll = function(){
     for(var objectIndex in this.objects){
         this.objects[objectIndex].update();
     }
--- appsUnitTests/threejsTest/server/GameStateTest.js
+++ appsUnitTests/threejsTest/server/GameStateTest.js
@@ -2,7 +2,7 @@ describe('GameState', function(){
     beforeEach(function(){
         this.gameState = new GameState();
 
-        this.objectSpy = {
+        this.objectSpy1 = {
             getId: function(){return "object1"},
             getPosition: function(){
                 return {
@@ -24,35 +24,68 @@ describe('GameState', function(){
             update: function(){}
         };
 
-        spyOn(this.objectSpy, "setPosition");
+        this.playerSpy1 = {
+            getId: function(){return "player1"},
+            getPosition: function(){
+                return {
+                    serialize: function(){return {x: 2, y: 0, z: 1}},
+                };
 
-        spyOn(this.objectSpy, "update");
+            },
+            setPosition: function(){},
+            update: function(){}
+        };
+
+        this.playerSpy2 = {
+            getId: function(){return "player2"},
+            getPosition: function(){
+                return {
+                    serialize: function(){return {x: 4, y: 2, z: 2}},
+                };
+            },
+            setPosition: function(){},
+            update: function(){}
+        };
+
+        spyOn(this.objectSpy1, "setPosition");
+        spyOn(this.playerSpy1, "setPosition");
+
+        spyOn(this.objectSpy1, "update");
         spyOn(this.objectSpy2, "update");
 
-        this.gameState.addObject(this.objectSpy);
+        this.gameState.addObject(this.objectSpy1);
         this.gameState.addObject(this.objectSpy2);
+
+        this.gameState.addPlayer(this.playerSpy1);
+        this.gameState.addPlayer(this.playerSpy2);
     });
 
-    it('can serialize objects names and positions', function(){
+    it('can serialize objects and players names and positions', function(){
         var currentGameState = this.gameState.serialize();
 
         expect(currentGameState.objects.object1.position).toEqual({x: 2, y: 0, z: 1});
         expect(currentGameState.objects.object1.id).toEqual("object1");
         expect(currentGameState.objects.object2.position).toEqual({x: 4, y: 2, z: 2});
         expect(currentGameState.objects.object2.id).toEqual("object2");
+
+        expect(currentGameState.players.player1.position).toEqual({x: 2, y: 0, z: 1});
+        expect(currentGameState.players.player1.id).toEqual("player1");
+        expect(currentGameState.players.player2.position).toEqual({x: 4, y: 2, z: 2});
+        expect(currentGameState.players.player2.id).toEqual("player2");
     });
 
-    it('updates object based on given specification', function(){
+    it('updates object and player based on given specification', function(){
     	this.gameState.updateObject({id: "object1", position: {}});
+    	this.gameState.updatePlayer({id: "player1", position: {}});
 
-        expect(this.objectSpy.setPosition).toHaveBeenCalled();
+        expect(this.objectSpy1.setPosition).toHaveBeenCalled();
+        expect(this.playerSpy1.setPosition).toHaveBeenCalled();
     });
 
     it('can update all contained object', function(){
-        this.gameState.updateAllObjects();
+        this.gameState.updateAll();
 
-        expect(this.objectSpy.update).toHaveBeenCalled();
+        expect(this.objectSpy1.update).toHaveBeenCalled();
         expect(this.objectSpy2.update).toHaveBeenCalled();
     });
-
 });
