06d6a03fdc7694a55d46238d7a8441cfaf3d2ab5
gemboj <gemboj99@gmail.com>
2016-04-17 22:39:42 +0200

    changed starting position of objects

--- apps/threejsTest/javascript/server/Server.js
+++ apps/threejsTest/javascript/server/Server.js
@@ -11,11 +11,13 @@ function Server(input){
 
     this.scene = new Scene();
 
+
+    var userAngle = 2*Math.PI/usernames.length;
     for(var i = 0; i < usernames.length; i++){
-        this.scene.addPlayer(new ServerPlayer(usernames[i], [0, 0, 0]));
+        this.scene.addPlayer(new ServerPlayer(usernames[i], [Math.cos(userAngle * i), Math.sin(userAngle * i), 0]));
     }
 
-    this.scene.addObject(new GameObject("object1", [1, 1, -1]));
+    this.scene.addObject(new GameObject("object1", [0, 0, 0]));
 
     this.mainLoop = new MainLoop(this.scene, this.communicator.broadcast.bind(this.communicator), 50);
 
