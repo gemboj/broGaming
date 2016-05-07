c9f3bbee54d99965e833454196eeeff85b2aa4d8
gemboj <gemboj99@gmail.com>
2016-05-07 11:59:32 +0200

    drawboard now lets users reconnect

--- apps/drawBoard/javascript/server/server.js
+++ apps/drawBoard/javascript/server/server.js
@@ -1,34 +1,45 @@
 function Server(input){
-    var channels = {};
+    this.channels = {};
+
+    this.createDataChannel = input.createDataChannel;
+    this.id = input.id;
+    this.showInfo = input.showInfo;
 
     for(var i = 0; i < input.usernames.length; ++i){
         var _i = i;
         var channel = input.createDataChannel(input.usernames[_i], input.id);
 
-        registerChannel(channel, input.usernames[i]);
+        this.registerChannel(channel, input.usernames[i]);
     }
 
-    function registerChannel(channel, username){
-        channels[username] = channel;
+}
 
-        channel.registerOnConnect(function(){
-            input.showInfo('server connected with ' + username);
-        });
+Server.prototype.registerChannel = function(channel, username){
+    var that = this;
 
-        channel.registerOnMessage(function(data){
-            for(username in channels){
-                channels[username].send(data)
-            }
-        });
+    this.channels[username] = channel;
 
-        channel.registerOnDisconnect(function(){
-            delete channels[username];
-            input.showInfo(username + ' disconnected');
-        });
+    channel.registerOnConnect(function(){
+        this.showInfo('server connected with ' + username);
+    });
 
-        channel.registerOnError(function(){
-            delete channels[username];
-            input.showInfo('could not connect with ' + username);
-        })
-    }
+    channel.registerOnMessage(function(data){
+        for(username in that.channels){
+            that.channels[username].send(data)
+        }
+    });
+
+    channel.registerOnDisconnect(function(){
+        delete that.channels[username];
+        this.showInfo(username + ' disconnected');
+    });
+
+    channel.registerOnError(function(){
+        delete that.channels[username];
+        this.showInfo('could not connect with ' + username);
+    })
+};
+
+Server.prototype.userJoined = function(username){
+    this.registerChannel(this.createDataChannel(username, this.id), username);
 };
